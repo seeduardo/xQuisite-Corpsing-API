039aaef8499faae363359118361ce1ba6ba2b3e8
seeduardo <romenflatiron@gmail.com>
2019-03-20 16:52:45 +0000

    Added mailer settings in config production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,25 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'xquisitecorpsing@gmail.com'}
+  config.action_mailer.default_url_options = { host: 'https://xquisite-corpsing.herokuapp.com/'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  user_name:            'xquisitecorpsing',
+  password:             'xQu1s1teCorps1ng',
+  authentication:       :plain,
+  enable_starttls_auto: true  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
