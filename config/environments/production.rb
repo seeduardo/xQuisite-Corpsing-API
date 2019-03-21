7a8f6617d15d882e7d6db9617857a5044a5a1e50
seeduardo <romenflatiron@gmail.com>
2019-03-21 16:03:20 +0000

    Changed default url during further mailer debugging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'xquisitecorpsing@gmail.com'}
-  config.action_mailer.default_url_options = { host: 'https://xquisite-corpsing.herokuapp.com/'}
+  config.action_mailer.default_url_options = { host: 'xquisite-corpsing.herokuapp.com/'}
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
